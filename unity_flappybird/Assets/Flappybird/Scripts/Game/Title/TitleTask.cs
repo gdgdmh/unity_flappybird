c14908e5efdbac83a59e40b562240daa4eb531fd
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2021-03-15 11:18:24 +0900

    当たった時の判定を追加、タイトルシーンを追加、タイトルからゲームシーンへ移動できるように追加

---
+++ unity_flappybird/Assets/Flappybird/Scripts/Game/Title/TitleTask.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleTask : MonoBehaviour
+{
+    private Mhl.ISingleTouchActionable touchAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchAction = new Mhl.SingleTouchActionEditor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(touchAction);
+        touchAction.Update();
+        if (touchAction.IsTouchEnded())
+        {
+            // 画面のどこかをタッチした
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
